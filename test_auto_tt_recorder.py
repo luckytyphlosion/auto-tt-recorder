45f877def681a3114bdcaae4ebca87d845ba10e2
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-06-30 19:41:10 -0400

    Add new tests, refactor test script.

--- test_auto_tt_recorder.py
+++ test_auto_tt_recorder.py
@@ -2,15 +2,11 @@ import yaml
 import subprocess
 import pathlib
 import random
+import glob
 
-test_config_filenames = (
-    #"test_comparison_fancy_xz_som_d1_folders1.yml",
-    "test_150cc_top10_region_filter_nohighlight_aq_standard_som_xz_main_ghost_auto_comparison_ghost_auto_no_bloom_folders2.yml",
-    #"test_200cc_fancy_som_d0_chadsoft_cached_folder_nested_folders3.yml",
-    #"test_200cc_top10_3rd_comparison_censors_regular_xyz_som_malaysia_no_bloom_no_blur_chadsoft_cached.yml",
-    #"test_150cc_default_drift_standard_som_no_bgm_lapmod.yml",
-    #"test_200cc_regular_som_d0_comparison_no_blur.yml",
-)
+test_config_indices = {
+    1, 2, 3, 4, 5, 6
+}
 
 class RegionFilenameAndName:
     __slots__ = ("filename", "name")
@@ -32,12 +28,17 @@ def main():
     pathlib.Path("temp").mkdir(exist_ok=True)
     pathlib.Path("test_vids").mkdir(exist_ok=True)
 
-    for config_filename in test_config_filenames:
-        with open(f"test_ymls/{config_filename}", "r") as f:
+    for config_filename in glob.glob("test_ymls/*.yml"):
+        config_basename = pathlib.Path(config_filename).name
+        config_index = int(config_basename.split("_", maxsplit=1)[0])
+        if config_index not in test_config_indices:
+            continue
+
+        with open(config_filename, "r") as f:
             config = yaml.safe_load(f)
 
         output_video_filepath_extension = config["output-video-filename"]
-        output_video_filepath_stem = pathlib.Path(config_filename).stem
+        output_video_filepath_stem = pathlib.Path(config_basename).stem
 
         if TEST_MULTIPLE_REGIONS:
             cur_region_filenames_and_names = region_filenames_and_names
@@ -45,19 +46,19 @@ def main():
             cur_region_filenames_and_names = (random.choice(region_filenames_and_names),)
 
         for region_filename_and_name in cur_region_filenames_and_names:
-            print(f"Testing region {region_filename_and_name.name}, config {config_filename}")
+            print(f"Testing region {region_filename_and_name.name}, config {config_basename}")
             config["iso-filename"] = f"../../RMCE 01/{region_filename_and_name.filename}"
             config["output-video-filename"] = f"test_vids/{output_video_filepath_stem}_{region_filename_and_name.name}.{output_video_filepath_extension}"
 
-            temp_config_filename = f"temp/{config_filename}"
+            temp_config_filename = f"temp/{config_basename}"
             with open(temp_config_filename, "w+") as f:
                 yaml.dump(config, f)
 
             completed_process = subprocess.run(("python3", "record_ghost.py", "-cfg", temp_config_filename))
             if completed_process.returncode == 0:
-                print(f"SUCCESS: region {region_filename_and_name.name}, config {config_filename}")
+                print(f"SUCCESS: region {region_filename_and_name.name}, config {config_basename}")
             else:
-                print(f"FAILURE: region {region_filename_and_name.name}, config {config_filename}")                
+                print(f"FAILURE: region {region_filename_and_name.name}, config {config_basename}")                
 
 if __name__ == "__main__":
     main()
